1a67b75c2b14811e19ec137492b26f726848d21c
yrwshj2s <gauravsharma3941@hotmail.com>
2023-08-26 20:21:24 +0800

    New game start files.

---
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Level")
+        {
+            Debug.Log("Ouch");
+        } 
+    }
+}
---
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("You win");
+    }
+}
